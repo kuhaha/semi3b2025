a72cdb508790b4c5d5b45bd4a10165d43fe65631
kinota93 <cheng@ip.kyusan-u.ac.jp>
2025-07-09 14:14:17 +0900

    Initial Commit

---
+++ hello.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title><?php echo "hello";?></title>
+</head><body>
+<?php 
+  echo "Hello, World!" ; 
+?>
+</body></html>
---
+++ helloworld.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head><meta charset="UTF-8"></head>
+<?php
+ echo date('Y-m-d');
+ echo "<h1>Hello, World!</h1>";
+ echo "はじまして、宜しくお願いします";
